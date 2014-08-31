6e0294c0198e88b99cf9a9bab6a99ea2831ce164
Seth Malaki <seth.malaki@gmail.com>
2014-09-01 02:25:07 +0800

    Updated parent instance to handle the events coming from their children

--- js/list.js
+++ js/list.js
@@ -1,5 +1,5 @@
 // Class input.js
-define(['jquery','components/list/input','db','hbs!templates/items'], function($, Input, DB, template) {
+define(['jquery','components/list/input','components/list/item','db','hbs!templates/items'], function($, Input, Item, DB, template) {
 	
 	var module = {
 		
@@ -7,13 +7,30 @@ define(['jquery','components/list/input','db','hbs!templates/items'], function($
 	var List = function(elem) {
 		if(!elem) return undefined;
 		var elem = $(elem);
+		var list = elem.find('ul');
 		elem.find('.actions input[type=text]').each(function() {
 			new Input(this, elem);
 		});
 
-		elem.on('todo:add', function(e, item) {
-			
-		});
+		elem
+			.on('todo:add', function(e, item) {
+				list.find('li.notice').remove();
+				var todo = new Item($(template([item])), elem)
+				$(todo.elem).appendTo(list);
+			})
+			.on('todo:del', function(e, uuid) {
+				// todo: remove first then 
+				if(!list.find('li').length) {
+					$(template([])).appendTo(list);
+				}
+			})
+			.on('todo:init', function() {
+
+				$(template([])).appendTo(list);	
+			})
+			;
+
+		elem.trigger('todo:init');
 		return module;
 	}
 	return List;
